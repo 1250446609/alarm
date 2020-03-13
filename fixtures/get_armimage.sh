da267e5d63e5f0bf3de7d466b22d992f75548fac
1250446609 <1250446609@users.noreply.github.com>
2020-03-13 13:27:52 +0800

    Create get_armimage.sh

---
+++ fixtures/get_armimage.sh
@@ -0,0 +1,17 @@
+sudo docker pull registry.cn-hangzhou.aliyuncs.com/hm_reponamespace/hm_repository: fabric-couchdb
+
+sudo docker pull registry.cn-hangzhou.aliyuncs.com/hm_reponamespace/hm_repository: fabric-tools
+
+
+sudo docker pull registry.cn-hangzhou.aliyuncs.com/hm_reponamespace/hm_repository: fabric-kafka
+
+
+sudo docker pull registry.cn-hangzhou.aliyuncs.com/hm_reponamespace/hm_repository: fabric-ccenv
+
+
+sudo docker pull registry.cn-hangzhou.aliyuncs.com/hm_reponamespace/hm_repository: fabric-orderer
+
+
+sudo docker pull registry.cn-hangzhou.aliyuncs.com/hm_reponamespace/hm_repository: fabric-peer
+
+sudo docker pull registry.cn-hangzhou.aliyuncs.com/hm_reponamespace/hm_repository: fabric-baseos
